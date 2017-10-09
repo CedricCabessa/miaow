dbeb3a10bbd30d481ecf04e04aabbc407c19b70c
Cédric Cabessa <ced@ryick.net>
2017-10-09 18:16:42 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ extern crate libmiaow;
 use libmiaow::dnsresolv::DnsResolv;
 use libmiaow::dnsparser::Resource;
 
+use std::{thread, time};
+
 fn main() {
     let dns = DnsResolv::new();
-    match dns.resolv_ptr("_http._tcp.local") {
-        Ok(dnslist) => {
-            let resource = Resource::parse_dns(dnslist).unwrap();
-            println!("{:?}", resource);
-        }
-        Err(e) => println!("fail {}", e),
-    };
+
+    loop {
+        match dns.resolv_ptr("_http._tcp.local") {
+            Ok(dnslist) => {
+                let resource = Resource::parse_dns(dnslist).unwrap();
+                println!("{:?}", resource);
+            }
+            Err(e) => println!("fail {}", e),
+        };
+        thread::sleep(time::Duration::from_secs(2));
+    }
 }
