c69c3ff048309bbf2fc76bc7ae389c0b6c78b4c1
Cédric Cabessa <ced@ryick.net>
2017-10-15 16:33:05 +0200

    introduce docopt for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
 extern crate libmiaow;
+extern crate docopt;
 
 use libmiaow::dnsresolv::DnsResolv;
 use libmiaow::dnsparser::Resource;
+use libmiaow::DnsError;
 
 use std::{thread, time};
 
-fn main() {
-    let dns = DnsResolv::new();
+use docopt::Docopt;
 
+const USAGE: &'static str = "
+miaow
+
+Usage:
+  miaow share <file>
+  miaow fetch <name>
+
+Options:
+  -h --help    Show this screen
+  --version    Show version
+";
+
+
+fn main() {
+    let args = Docopt::new(USAGE)
+        .and_then(|d| d.argv(std::env::args().into_iter()).parse())
+        .unwrap_or_else(|e| e.exit());
+
+    if args.get_bool("fetch") {
+        let name = args.get_str("<name>");
+        fetch(name);
+    } else if args.get_bool("share") {
+        println!("share is not yet implemented");
+    }
+}
+
+fn fetch(name :&str) {
+    let dns = DnsResolv::new();
     loop {
         match dns.resolv_ptr("_http._tcp.local") {
             Ok(dnslist) => {
-                let resource = Resource::parse_dns(dnslist).unwrap();
-                println!("{:?}", resource);
+                match Resource::parse_dns(dnslist) {
+                    Ok(resource) => println!("{:?}", resource),
+                    Err(err) => match err {
+                        DnsError::InvalidResource() => { println!("invalid"); continue},
+                        err => println!("{:?}", err),
+                    }
+                }
             }
             Err(e) => println!("fail {}", e),
         };
